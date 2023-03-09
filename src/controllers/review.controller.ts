2d1b5f831ce4901c6b414c3222e650d2c09ff25f
redfaceman <118166327+redfaceman@users.noreply.github.com>
2023-03-09 09:04:52 +0900

    feat: "review CRUD 추가"

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { ReviewService } from '../services/review.service';
+import { CreateReviewDto } from '../dto/create-review.dto';
 
 @Controller('review')
-export class ReviewController {}
+export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
+  @Get('/:contentId')
+  async getReviewSpec(@Param('contentId') contentId: number) {
+    const review = await this.reviewService.getReviewSpec(contentId);
+
+    return review;
+  }
+
+  @Post('/')
+  createReview(@Body() data: CreateReviewDto) {
+    return this.reviewService.createReview(
+      data.contentId,
+      data.userId,
+      data.rating,
+      data.review
+    );
+  }
+
+  @Put('/performance/:prfmId')
+  updatePerform(
+    @Param('prfmId') prfmId: number,
+    @Body() data: CreateReviewDto
+  ) {
+    return this.reviewService.updateReview(
+      data.contentId,
+      data.userId,
+      data.rating,
+      data.review
+    );
+  }
+}
--- src/services/review.service.ts
+++ src/services/review.service.ts
@@ -6,6 +6,30 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class ReviewService {
   constructor(
-    @InjectRepository(Review) private contentRepository: Repository<Review>
+    @InjectRepository(Review) private reviewRepository: Repository<Review>
   ) {}
+
+  async getReviewSpec(contentId: number) {
+    const review = await this.reviewRepository.findOne({
+      where: {
+        deletedAt: null,
+        id: contentId,
+      },
+    });
+    return { review };
+  }
+
+  createReview(
+    contentId: number,
+    userId: number,
+    rating: number,
+    review: string
+  ) {
+    this.reviewRepository.insert({
+      contentId,
+      userId,
+      rating,
+      review,
+    });
+  }
 }
