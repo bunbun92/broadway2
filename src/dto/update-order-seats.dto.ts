071803f07e8dc480809feb0bd5d3a534665502ca
bunbun92 <wjdwlfk12@naver.com>
2023-03-13 18:02:41 +0900

    Feat: dto 추가

--- src/dto/update-order-seats.dto.ts
+++ src/dto/update-order-seats.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsArray } from 'class-validator';
 import { CreateOrderSeatsDto } from './create-order-seats.dto';
 
-export class UpdateOrderSeatsDto extends PartialType(CreateOrderSeatsDto) {}
+export class UpdateOrderSeatsDto extends PartialType(CreateOrderSeatsDto) {
+  @IsArray()
+  readonly seatsBefore: Array<string>;
+
+  @IsArray()
+  readonly seatsAfter: Array<string>;
+}
