fb32bddb4c894816491f07cb11be89ba03e8165a
kihanGIM <58027260+kihanGIM@users.noreply.github.com>
2023-03-14 23:49:45 +0900

    Chore: 디렉토리 테테스트

--- src/renderers/router.module.ts
+++ src/renderers/router.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { RenderContentModule } from './content/render-content.module';
 import { RenderOrderSeatsModule } from './orderSeats/render-order-seats.module';
 import { RouterController } from './router.controller';
 
 @Module({
-  imports: [RenderOrderSeatsModule],
+  imports: [RenderOrderSeatsModule, RenderContentModule],
   controllers: [RouterController],
 })
 export class RouterModule {}
