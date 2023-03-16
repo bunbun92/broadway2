cded197bdae702c5a736f6a45d5206863c469ff4
bhkim0707 <bhkim0707@gmail.com>
2023-03-17 04:54:34 +0900

    featuer: theater info CRUD API

--- src/controllers/theaters.controller.ts
+++ src/controllers/theaters.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreatePriceInfoDto } from 'src/dto/create-price-info.dto';
+import { CreateSeatsInfoDto } from 'src/dto/create-seats-info.dto';
+import { CreateTheaterDto } from 'src/dto/create-theater.dto';
 import { TheatersService } from 'src/services/theaters.service';
 
 @Controller('theaters')
@@ -6,7 +9,71 @@ export class TheatersController {
   constructor(private readonly theatersService: TheatersService) {}
 
   @Get('/')
+  async getAllTheaterList() {
+    return await this.theatersService.getAllTheaterList();
+  }
+
+  @Get('/info')
   async getAllTheaters() {
-    return await this.theatersService.getAllTheaters();
+    return await this.theatersService.getAllTheaterInfo();
+  }
+
+  @Get('/myList/:userId')
+  async getMyTheaterInfo(@Param('userId') userId: number) {
+    return await this.theatersService.getMyTheaterInfo(userId);
+  }
+
+  @Post('/createTheater/:userId')
+  createTheaterInfo(
+    @Param('userId') userId: number,
+    @Body() data: CreateTheaterDto
+  ) {
+    this.theatersService.createTheaterInfo(data.theater, userId);
+  }
+
+  @Delete('/deleteTheater/:theaterId')
+  deleteTheaterInfo(@Param('theaterId') theaterId: number) {
+    this.theatersService.deleteTheaterInfo(theaterId);
+  }
+
+  @Get('/seatsList/:theaterId')
+  async getSeatsInfoByTheaterId(@Param('theaterId') theaterId: number) {
+    return await this.theatersService.getSeatsInfoByTheaterId(theaterId);
+  }
+
+  @Post('/createSeats')
+  createSeatsInfo(@Body() data: CreateSeatsInfoDto) {
+    this.theatersService.createSeatsInfo(
+      data.theaterId,
+      data.userId,
+      data.rowMax,
+      data.columnMax
+    );
+  }
+
+  @Delete('/deleteSeats/:theaterId')
+  deleteSeatsInfo(@Param('theaterId') theaterId: number) {
+    this.theatersService.deleteSeatsInfo(theaterId);
+  }
+
+  @Get('/priceInfo/:performId')
+  async getPriceInfoByPerformId(@Param('performId') performId: string) {
+    return await this.theatersService.getPriceInfoByPerformId(performId);
+  }
+
+  @Post('/createPriceInfo')
+  createPriceInfo(@Body() data: CreatePriceInfoDto) {
+    this.theatersService.createPriceInfo(
+      data.grade,
+      data.price,
+      data.performId,
+      data.theaterId,
+      data.userId
+    );
+  }
+
+  @Delete('/deletePriceInfo/:performId')
+  deletePriceInfo(@Param('performId') performId: string) {
+    this.theatersService.deletePriceInfo(performId);
   }
 }
--- src/services/theaters.service.ts
+++ src/services/theaters.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { KopisApi } from 'src/entities/kopisApi.entity';
 import { Theater } from 'src/entities/theater-info.entity';
 import { PriceInfo } from 'src/entities/theater-price.entity';
 import { SeatsInfo } from 'src/entities/theater-seats.entity';
@@ -12,13 +13,100 @@ export class TheatersService {
     @InjectRepository(PriceInfo)
     private priceInfoRepository: Repository<PriceInfo>,
     @InjectRepository(SeatsInfo)
-    private seatsInfoRepository: Repository<SeatsInfo>
+    private seatsInfoRepository: Repository<SeatsInfo>,
+    @InjectRepository(KopisApi) private kopisApiRepository: Repository<KopisApi>
   ) {}
 
-  async getAllTheaters() {
+  async getAllTheaterInfo() {
     return await this.theaterRepository.find({
       where: { deletedAt: null },
       relations: ['users'],
     });
+  }
+
+  async getMyTheaterInfo(userId: number) {
+    return await this.theaterRepository.find({
+      where: { userId, deletedAt: null },
+      relations: ['users'],
+    });
+  }
+
+  async getAllTheaterList() {
+    const theaterList = await this.kopisApiRepository
+      .createQueryBuilder('kopisApi')
+      .select('DISTINCT(kopisApi.theater)', 'theater')
+      .orderBy('theater')
+      .getRawMany();
+
+    return theaterList.map(theater => theater.theater);
+  }
+
+  createTheaterInfo(theater: string, userId: number) {
+    this.theaterRepository.insert({ theater, userId });
+  }
+
+  deleteTheaterInfo(id: number) {
+    this.theaterRepository.softDelete({ id });
+  }
+
+  async getSeatsInfoByTheaterId(theaterId: number) {
+    return await this.seatsInfoRepository.find({
+      where: { theaterId, deletedAt: null },
+      order: { seat: 'ASC' },
+    });
+  }
+
+  createSeatsInfo(
+    theaterId: number,
+    userId: number,
+    rowMax: number,
+    columnMax: number
+  ) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+    for (let i = 0; i < rowMax; i++) {
+      for (let j = 0; j < columnMax; j++) {
+        const rowIndex = alphabet[i];
+        const formattedColumnIndex = j < 9 ? `0${j + 1}` : `${j + 1}`;
+        const seat = `${rowIndex}${formattedColumnIndex}`;
+
+        this.seatsInfoRepository.insert({
+          theaterId,
+          seat,
+          userId,
+        });
+      }
+    }
+  }
+
+  deleteSeatsInfo(theaterId: number) {
+    this.seatsInfoRepository.softDelete({ theaterId });
+  }
+
+  async getPriceInfoByPerformId(performId) {
+    return await this.priceInfoRepository.find({
+      where: { performId, deletedAt: null },
+      relations: ['kopisApi'],
+    });
+  }
+
+  createPriceInfo(
+    grade: number,
+    price: number,
+    performId: string,
+    theaterId: number,
+    userId: number
+  ) {
+    this.priceInfoRepository.insert({
+      grade,
+      price,
+      performId,
+      theaterId,
+      userId,
+    });
+  }
+
+  deletePriceInfo(performId: string) {
+    this.priceInfoRepository.softDelete({ performId });
   }
 }
