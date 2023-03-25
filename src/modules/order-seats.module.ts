51e9016800cbd6a58a158029bf219417842adcc4
bunbun92 <wjdwlfk12@naver.com>
2023-03-26 00:51:11 +0900

    Feat: 결제건 별 예매내역 출력, 취소 추가 & Refactor: 좌석별 가격 차등입력

--- src/modules/order-seats.module.ts
+++ src/modules/order-seats.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Content } from 'src/entities/content.entity';
 import { OrderList } from 'src/entities/order-list.entity';
+import { Order } from 'src/entities/order.entity';
 import { Seat } from 'src/entities/seats.entity';
 import { TimeSale } from 'src/entities/time-sale.entity';
 import { OrderSeatsController } from '../controllers/order-seats.controller';
 import { OrderSeatsService } from '../services/order-seats.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Content, Seat, OrderList, TimeSale])],
+  imports: [
+    TypeOrmModule.forFeature([Content, Seat, OrderList, TimeSale, Order]),
+  ],
   controllers: [OrderSeatsController],
   providers: [OrderSeatsService],
 })
